1c3590f0e19fef7a39b157e88b907c3f6de3741c
padamchopra <jb.padamchopra@gmail.com>
2019-05-04 20:23:20 +0530

    hostonheroku

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <head>
 
+    <link rel="icon" href="tarea-logo.png" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
